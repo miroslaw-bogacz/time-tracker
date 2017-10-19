3f621e7d6980849738d47358cfa135b16eb81669
miroslaw.bogacz <miroslaw.bogacz@hotmail.com>
2017-10-19 15:39:19 +0200

    add logout and redirect to the account create after the logout

--- src/app/account/actions/account.actions.ts
+++ src/app/account/actions/account.actions.ts
@@ -1,11 +1,17 @@
 import { Action } from '@ngrx/store';
 
 export const UPDATE = '[Account] update account';
+export const LOGOUT = '[Account] logout';
 
 export class Update implements Action {
   readonly type = UPDATE;
   constructor(public payload: any) {}
 }
 
+export class Logout implements Action {
+  readonly type = LOGOUT;
+}
+
 export type AccountActions =
-  Update;
+  Update
+  | Logout;
