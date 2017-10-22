086fbc4bedfabac9a6f95f368d4711d12f283689
przemkow <przemkow92@gmail.com>
2017-10-22 17:12:45 +0200

    Add electron menu to enable usage of basic keyboard shortcuts

--- electron/main.js
+++ electron/main.js
@@ -63,7 +63,9 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
-  })
+  });
+
+  require('./menu');
 }
 
 // This method will be called when Electron has finished
