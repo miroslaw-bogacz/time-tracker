108ce97e68123b61c1e284bf32871a1cc454c3b9
Mirosław Bogacz <miroslaw.bogacz@hotmail.com>
2017-10-29 12:44:10 +0100

    (#40) add unit tests for getErrorsFromPayload
    resolved #41

---
+++ src/app/core/helpers/get-errors-from-payload.spec.ts
@@ -0,0 +1,23 @@
+import { getErrorsFromPayload } from './get-errors-from-payload';
+
+describe('getErrorsFromPayload', () => {
+
+  describe('when call with payload with errors', () => {
+    it('should return array errors', () => {
+      const payload = { errorMessages: [ 'error 1', 'error 2' ] };
+      const result = getErrorsFromPayload(payload);
+
+      expect(result).toEqual(payload.errorMessages);
+    });
+  });
+
+  describe('when call without payload errors', () => {
+    it('should return empty array', () => {
+      const payload = {};
+      const result = getErrorsFromPayload(payload);
+      console.log(result);
+      expect(result).toEqual([]);
+    });
+  });
+
+});
