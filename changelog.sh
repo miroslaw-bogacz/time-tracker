56523f3f98754ac4ab0b15ae26ef66313b25a3c1
miroslaw.bogacz <miroslaw.bogacz@hotmail.com>
2017-11-06 12:20:54 +0100

    development/#95 add changelogs generator

---
+++ changelog.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+git_logs=$(git log `git describe --tags --abbrev=0 HEAD^`..HEAD --oneline)
+
+git_logs_features=$(grep 'feature/' --ignore-case <<< "$git_logs")
+
+if [ "$git_logs_features" ]
+then
+    echo ""
+    echo "## Features"
+    while IFS= read -r line
+    do
+        echo "- $line"
+    done <<< "$git_logs_features"
+fi
+
+git_logs_bugfixes=$(grep 'bugfix/' --ignore-case <<< "$git_logs")
+
+if [ "$git_logs_bugfixes" ]
+then
+    echo ""
+    echo "## Bug fixes"
+    while IFS= read -r line
+    do
+        echo "- $line"
+    done <<< "$git_logs_bugfixes"
+fi
+
+git_logs_developments=$(grep 'development/' --ignore-case <<< "$git_logs")
+
+if [ "$git_logs_developments" ]
+then
+    echo ""
+    echo "## Development"
+    while IFS= read -r line
+    do
+        echo "- $line"
+    done <<< "$git_logs_developments"
+fi
