61a62f7c11f311d8eb4ddeebe368b963b3d30d90
Mirosław Bogacz <miroslaw.bogacz@hotmail.com>
2017-11-06 13:44:18 +0100

    development/#85 add unit tests for AppComponent
    resolve #85

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,17 +5,29 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Store } from '@ngrx/store';
 import { JiraRequestOptionsService } from './shared/jira-api/services/jira-request-options.service';
+import * as accountActions from './account/actions/account.actions';
 
 describe('AppComponent', () => {
+  let fixture;
+  let app;
+  let storeBehaviorSubject;
+
   const mockedStore: any = jasmine
-    .createSpyObj('mockedStore', [ 'select' ]);
+    .createSpyObj('mockedStore', [ 'select', 'dispatch' ]);
 
   const mockedJiraRequestOptionsService = jasmine
     .createSpyObj('mockedJiraRequestOptionsService', [ 'setOptions' ]);
 
-  beforeEach(async(() => {
-    mockedStore.select.and.returnValue(new BehaviorSubject({}));
+  const mockedAccount = {
+    www: 'http://www.test-www.com',
+    username: 'test-username',
+    token: 'test-token',
+  };
 
+  beforeEach(async(() => {
+    storeBehaviorSubject = new BehaviorSubject(mockedAccount);
+
+    mockedStore.select.and.returnValue(storeBehaviorSubject);
 
     TestBed.configureTestingModule({
       declarations: [
@@ -31,9 +43,39 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
     expect(app).toBeTruthy();
-  }));
+  });
+
+  describe('when store return account', () => {
+
+    it('should be call JiraRequestOptionsService.setOptions', () => {
+      expect(mockedJiraRequestOptionsService.setOptions)
+        .toHaveBeenCalledWith({
+          protocol: 'http',
+          port: null,
+          domain: 'www.test-www.com',
+          authentication: 'Basic Auth',
+          token: 'test-token',
+        });
+    });
+  });
+
+  describe('when logout has been call', () => {
+
+    beforeEach(() => {
+      app.logout();
+    });
+
+    it('should be call dispatch with action', () => {
+      expect(mockedStore.dispatch).toHaveBeenCalledWith(new accountActions.Logout());
+    });
+
+  });
 });
